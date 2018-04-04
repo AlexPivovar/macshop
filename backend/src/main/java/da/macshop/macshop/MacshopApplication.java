0b3c0f6df0fc5dfaf2ef2ca8e55e967624044b83
A. Pivovar <pivovar.alex96@gmail.com>
2018-04-05 00:06:33 +0300

    Init commit

---
+++ backend/src/main/java/da/macshop/macshop/MacshopApplication.java
@@ -0,0 +1,12 @@
+package da.macshop.macshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacshopApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/da/macshop/macshop/MacshopApplicationTests.java
@@ -0,0 +1,16 @@
+package da.macshop.macshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MacshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
